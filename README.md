61e345716e006f63d67a2a157c580a121856d85d
wellington moreira <wsantos08@hotmail.com>
2023-12-05 20:15:08 -0300

    iniciando repositorio

---
+++ README.md
@@ -0,0 +1,5 @@
+# Data Cleaning 
+
+A limpeza de dados é o processo de encontrar e corrigir dados imprecisos em um conjunto de dados. Isso envolve identificar e remover inconsistências, preencher campos vazios, corrigir erros estruturais e garantir que os dados sejam precisos, completos e consistentes.
+
+Nesse projeto vamos utilizar a linguagem R
--- scripts/data-cleaning.R
+++ scripts/data-cleaning.R
@@ -6,8 +6,9 @@ setwd("D:/Projetos/R/limpeza-tratamento/dados")
 dados <- read.csv("Churn.csv", sep=";",stringsAsFactors = T ,na.strings = "")
 
 # conhecendo os dados
-dim(dados)
-
 head(dados)
-
 summary(dados)
+
+# renomeando colunas
+colnames(dados) = c("Id","Score","Estado","Genero","Idade","Patrimonio","Saldo","Produtos","TemCartCredito","Ativo","Salario","Saiu")
+head(dados)
